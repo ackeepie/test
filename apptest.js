ae83638c42989de650363f714f8221a51993d2f3
testwpt <testwpt@users.noreply.github.com>
2015-01-09 20:58:39 -0500

    Update apptest.js

--- apptest.js
+++ apptest.js
@@ -1,1 +1,8 @@
+var http      = require('http');
+var express   = require('express');
+
+var app    = express();
+console.log('starting Express (NodeJS) Web server');
+app.listen(8080);
+console.log('Webserverlistening on port 8080');
 alert("test")
